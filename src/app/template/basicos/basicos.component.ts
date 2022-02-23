1382445b1ad6acc7aabd58146a0dda1e8c1e08ea
Alan Cortes <ing.ti.upmh@gmail.com>
2022-02-23 14:51:19 -0600

    Formularios por template
    * Creación de módulos necesarios
    * Creación de componentes necesarios
    * Menu de la aplicación
    * Diseño de formulario y uso de FormsModule
    * Mostrar mensajes de error y uso de ViewChild
    * Limpiar formulario y directiva personalizada

---
+++ src/app/template/basicos/basicos.component.ts
@@ -0,0 +1,55 @@
+import { invalid } from '@angular/compiler/src/render3/view/util';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [
+  ]
+})
+export class BasicosComponent implements OnInit {
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  initForm = {
+
+    producto: 'RTX 4080',
+    precio: 10,
+    existencias: 10
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // guardar(miFormulario: NgForm) {
+  //   console.log(miFormulario);
+
+  // }
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid &&
+           this.miFormulario?.controls['producto']?.touched;
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.touched &&
+            this.miFormulario?.controls['precio']?.value < 0;
+  }
+
+  guardar() {
+    console.log(this.miFormulario);
+    console.log('Posteo correcto');
+    this.miFormulario.resetForm({
+      precio: 0,
+      existencias: 0
+    });
+
+
+
+  }
+
+}
