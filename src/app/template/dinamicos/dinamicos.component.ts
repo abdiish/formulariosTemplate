6d6a1166ad5ada3e54b7cef6796d1f21aa00fb4a
Alan Cortes <ing.ti.upmh@gmail.com>
2022-02-23 16:53:14 -0600

    Template dinámicos
    * Agregar elementos de forma dinámica
    * Eliminar elemntos de forma dinámica
    * Uso de arreglo para agregar favoritos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,16 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent{
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Fernando',
+    favoritos: [
+      {
+        id: 1, nombre: 'Favorito 1'
+      },
+      {
+        id: 2, nombre: 'Favorito 2'
+      }
+    ]
+  }
+
+  guardar() {
+    console.log('Formulario posteado');
+
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarJuego() {
+
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push(nuevoFavorito);
+    this.nuevoJuego = '';
+
   }
 
 }
