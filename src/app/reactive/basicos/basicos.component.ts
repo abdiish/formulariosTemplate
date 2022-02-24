756ea2a4a6de5a508412cff92a7d726b23bc0476
Alan Cortes <ing.ti.upmh@gmail.com>
2022-02-24 16:24:40 -0600

    Uso de boton submit, reset y manejo de mensajes de error

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -7,7 +7,7 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
   styles: [
   ]
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
 
   // miFormulario: FormGroup = new FormGroup({
 
@@ -30,9 +30,38 @@ export class BasicosComponent {
 
   constructor(private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+
+    // Establecer valores a formulario setValue
+    // this.miFormulario.setValue({
+    //   nombre: 'RTX 4080',
+    //   precio: 1600,
+    //   existencias: 6
+    // });
+
+    // Establecer valores a formulario reset
+    this.miFormulario.reset({
+      nombre: 'RTX 4080',
+      precio: 1600,
+    });
+  }
+
   campoEsValido(campo: string) {
     return this.miFormulario.controls[campo].errors &&
            this.miFormulario.controls[campo].touched
+  }
+
+
+  guardar() {
+
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+
   }
 
 }
